ec36834721feec2cef0f4331c9d6765f7ce54993
deka9584 <sala_2000@yahoo.it>
2025-06-12 16:11:17 +0200

    Added footer

---
+++ src/components/WsFooter.jsx
@@ -0,0 +1,11 @@
+function WsFooter () {
+    return (
+        <footer className="bg-black text-white">
+            <div className="max-w-7xl mx-auto px-xl py-lg">
+                <span className="text-lg">Andrea Sala | Web Portfolio</span>
+            </div>
+        </footer>
+    );
+}
+
+export default WsFooter;
