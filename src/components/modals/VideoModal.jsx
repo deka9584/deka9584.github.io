8cd514f61c72ff166f69451a12602f9fb95dee80
deka9584 <sala_2000@yahoo.it>
2025-06-17 00:09:31 +0200

    Video modal

---
+++ src/components/modals/VideoModal.jsx
@@ -0,0 +1,50 @@
+import NiceModal from "@ebay/nice-modal-react";
+import { useEffect, useRef, useState } from "react";
+
+const VideoModal = NiceModal.create(({ src }) => {
+    const modal = NiceModal.useModal();
+    const videoRef = useRef();
+    const [isPaused, setIsPaused] = useState(false);
+
+    const togglePlayPause = () => {
+        isPaused ? videoRef.current?.play() : videoRef.current?.pause();
+    }
+
+    const showFullscreen = () => {
+        videoRef.current?.requestFullscreen();
+    }
+
+    useEffect(() => {
+        const keydownHandler = (event) => {
+            if (event.key?.toLowerCase() === "escape") {
+                modal.remove();
+            } 
+        }
+
+        window.addEventListener("keydown", keydownHandler);
+        return () => window.removeEventListener("keydown", keydownHandler);
+    });
+
+    return (
+        <div className="fixed inset-0 size-full bg-black/75 z-30">
+            <dialog className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-white shadow w-full max-w-3xl" open>
+                <button type="button" className="video-controls absolute top-0 right-0 z-10" onClick={() => modal.remove()}>
+                    <i className="bi bi-x-lg"></i>
+                </button>
+                <video src={src} ref={videoRef} onPlay={() => setIsPaused(false)} onPause={() => setIsPaused(true)} autoPlay></video>
+                <button type="button" onClick={togglePlayPause} className="video-controls absolute bottom-0 left-0 z-10">
+                    {isPaused ? (
+                        <i className="bi bi-play"></i>
+                    ) : (
+                        <i className="bi bi-pause"></i>
+                    )}
+                </button>
+                <button type="button" onClick={showFullscreen} className="video-controls absolute bottom-0 right-0 z-10">
+                    <i className="bi bi-arrows-fullscreen"></i>
+                </button>
+            </dialog>
+        </div>
+    );
+});
+
+export default VideoModal;
