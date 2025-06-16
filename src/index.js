1c04ce325442762e2b868f2419aec801eb025d82
deka9584 <sala_2000@yahoo.it>
2025-06-17 00:09:23 +0200

    Added modal library

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import NiceModal from '@ebay/nice-modal-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-    <App />
+    <NiceModal.Provider>
+      <App />
+    </NiceModal.Provider>
   </HashRouter>
 );
 
