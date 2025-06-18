51fe18f6a147d35bb66fe827adad0c187d9362e7
deka9584 <sala_2000@yahoo.it>
2025-06-18 16:46:18 +0200

    Added video loading spinner

--- src/components/modals/VideoModal.jsx
+++ src/components/modals/VideoModal.jsx
@@ -1,10 +1,14 @@
 import NiceModal from "@ebay/nice-modal-react";
 import { useEffect, useRef, useState } from "react";
+import { ReactSVG } from "react-svg";
+import loaderSpinner from "../../assets/loader-spinner.svg";
 
 const VideoModal = NiceModal.create(({ src }) => {
     const modal = NiceModal.useModal();
     const videoRef = useRef();
     const [isPaused, setIsPaused] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     const togglePlayPause = () => {
         isPaused ? videoRef.current?.play() : videoRef.current?.pause();
@@ -12,6 +16,21 @@ const VideoModal = NiceModal.create(({ src }) => {
 
     const showFullscreen = () => {
         videoRef.current?.requestFullscreen();
+    }
+
+    const handleLoadStart = () => {
+        setError(null);
+        setIsLoading(true);
+    }
+
+    const handleCanPlay = () => {
+        setError(null);
+        setIsLoading(false);
+    }
+
+    const handleError = () => {
+        setError("Loading error");
+        setIsLoading(false);
     }
 
     useEffect(() => {
@@ -31,7 +50,28 @@ const VideoModal = NiceModal.create(({ src }) => {
                 <button type="button" className="video-controls absolute top-0 right-0 z-10" onClick={() => modal.remove()}>
                     <i className="bi bi-x-lg"></i>
                 </button>
-                <video src={src} ref={videoRef} onPlay={() => setIsPaused(false)} onPause={() => setIsPaused(true)} autoPlay></video>
+                {isLoading && (
+                    <div className="absolute inset-0 size-full flex items-center justify-center">
+                        <ReactSVG src={loaderSpinner} aria-label="Loading" className="size-2xl animate-spin"/>
+                    </div>
+                )}
+                {error && (
+                    <div className="absolute inset-0 size-full flex flex-col items-center justify-center">
+                        <i className="bi bi-exclamation-circle text-alert text-2xl"></i>
+                        <span className="mt-base text-sm">{error}</span>
+                    </div>
+                )}
+                <video
+                    src={src}
+                    ref={videoRef}
+                    onPlay={() => setIsPaused(false)}
+                    onPause={() => setIsPaused(true)}
+                    onLoadStart={handleLoadStart}
+                    onCanPlayThrough={handleCanPlay}
+                    onError={handleError}
+                    onErrorCapture={(e) => console.log(e)}
+                    autoPlay
+                />
                 <button type="button" onClick={togglePlayPause} className="video-controls absolute bottom-0 left-0 z-10">
                     {isPaused ? (
                         <i className="bi bi-play"></i>
